6bd3348831556a849cb44564878938765267e945
Jonathan Perkin <jonathan@perkin.org.uk>
2012-02-29 13:15:21 +0000

    Update comments.

--- config.js
+++ config.js
@@ -1,14 +1,28 @@
 var config = {}
 
-// This both configures which probes to run, and their frequency.
+/*
+ * Configure probes to run, and their frequency in seconds.
+ *
+ * Note that for RRD::Simple output you should match the frquency to the
+ * step size of your RRD databases, else they will be filled with NaNs.
+ */
 config.probes = [
   ['system.uptime',         '300'],
   [/.*/,                     '60'],
 ]
 
-// Possible options: ['console', 'graphite', 'rrd-simple']
+/*
+ * Possible output formats:
+ *
+ *   'console':     Dump to stdout
+ *   'graphite':    Post to a graphite server
+ *   'rrd-simple':  HTTP POST to an RRD::Simple server
+ */
 config.output_format = 'console';
 
+/*
+ * Options for RRD::Simple
+ */
 config.rrdsimple = {};
 config.rrdsimple.host = 'rrd.me.uk';
 config.rrdsimple.port = 80;
