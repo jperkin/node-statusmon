baaeb33e39938f16627ab03380d0a41598a56bce
Jonathan Perkin <jonathan@perkin.org.uk>
2012-02-29 19:54:15 +0000

    Move system.mpstat out into its own cpu.js probe and put it under a cpu.* namespace which better matches other probes, and allows for future growth.

---
+++ probes/cpu.js
@@ -0,0 +1,42 @@
+/*
+ * CPU statistics
+ */
+
+var exec = require('child_process').exec;
+
+/*
+ * Unfortunately the majority of systems are en_US based, so we go with
+ * the wrong spelling of 'utilisation' for the least hassle ;)
+ */
+function get_cpu_utilization(callback)
+{
+  switch (process.platform) {
+  case 'linux':
+    exec('mpstat -u -P ALL 1 1', function (err, stdout, stderr) {
+      var capture = 0;
+      stdout.split('\n').forEach(function (line) {
+        var ret = {};
+        if (line.length === 0) {
+          capture = (capture) ? 0 : 1;
+          return;
+        }
+        if (capture) {
+          var vals = line.split(/\s+/);
+          if (!vals[1].match(/\d+/)) {
+            return;
+          }
+          ret['cpu.utilization.cpu' + vals[1] + '.user'] = vals[2];
+          ret['cpu.utilization.cpu' + vals[1] + '.system'] = vals[4];
+          ret['cpu.utilization.cpu' + vals[1] + '.iowait'] = vals[5];
+          ret['cpu.utilization.cpu' + vals[1] + '.idle'] = vals[10];
+          callback(ret);
+        }
+      });
+    });
+    break;
+  }
+}
+
+module.exports.probes = {
+  'cpu.utilization': get_cpu_utilization,
+}
--- probes/system.js
+++ probes/system.js
@@ -5,35 +5,6 @@
 var os = require('os');
 var exec = require('child_process').exec;
 
-function get_mpstat(callback)
-{
-  switch (process.platform) {
-  case 'linux':
-    exec('mpstat -u -P ALL 1 1', function (err, stdout, stderr) {
-      var capture = 0;
-      stdout.split('\n').forEach(function (line) {
-        var ret = {};
-        if (line.length === 0) {
-          capture = (capture) ? 0 : 1;
-          return;
-        }
-        if (capture) {
-          var vals = line.split(/\s+/);
-          if (!vals[1].match(/\d+/)) {
-            return;
-          }
-          ret['system.mpstat.cpu' + vals[1] + '.user'] = vals[2];
-          ret['system.mpstat.cpu' + vals[1] + '.system'] = vals[4];
-          ret['system.mpstat.cpu' + vals[1] + '.iowait'] = vals[5];
-          ret['system.mpstat.cpu' + vals[1] + '.idle'] = vals[10];
-          callback(ret);
-        }
-      });
-    });
-    break;
-  }
-}
-
 function get_loadavg(callback)
 {
   var vals = os.loadavg();
@@ -74,7 +45,6 @@ function get_users(callback)
 }
 
 module.exports.probes = {
-  'system.mpstat': get_mpstat,
   'system.loadavg': get_loadavg,
   'system.uptime': get_uptime,
   'system.users': get_users,
